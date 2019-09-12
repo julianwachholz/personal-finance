370ff341f74cd458387dbd39d326e80fac992a66
Julian Wachholz <julian.wachholz@styx7.com>
2019-09-12 12:37:16 +0200

    Add categories app

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,32 +1,27 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from djmoney.models.fields import MoneyField
 
 
-class Account(models.Model):
+class Category(models.Model):
     """
-    A financial account holds transactions.
+    A category classifies budgets or transactions.
 
     """
 
     name = models.CharField(verbose_name=_("name"), max_length=100)
 
-    institution = models.CharField(verbose_name=_("institution"), max_length=100)
-
     user = models.ForeignKey(to="auth.User", on_delete=models.CASCADE)
-
-    balance = MoneyField(
-        max_digits=10, decimal_places=2, currency_field_name="currency"
-    )
 
     icon = models.CharField(verbose_name=_("icon"), max_length=100)
 
     pos = models.PositiveSmallIntegerField(default=0, db_index=True)
 
+    parent = models.ForeignKey(to="self", on_delete=models.CASCADE)
+
     class Meta:
-        verbose_name = _("account")
-        verbose_name_plural = _("accounts")
-        ordering = ("user", "institution", "name")
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+        ordering = ("user", "pos", "name")
 
     def __str__(self):
         return self.name
--- shinywaffle/settings.py
+++ shinywaffle/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.budgets",
+    "apps.categories",
     "apps.accounts",
     "apps.transactions",
 ]
