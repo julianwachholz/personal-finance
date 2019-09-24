f329736b1564d86c166aeba1d8c4436f3a2ee2f1
Julian Wachholz <julian.wachholz@styx7.com>
2019-09-24 13:10:28 +0200

    Add repr to Account

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -16,7 +16,10 @@ class Account(models.Model):
     user = models.ForeignKey(to="auth.User", on_delete=models.CASCADE)
 
     balance = MoneyField(
-        max_digits=10, decimal_places=2, currency_field_name="currency"
+        verbose_name=_("balance"),
+        max_digits=10,
+        decimal_places=2,
+        currency_field_name="currency",
     )
 
     icon = models.CharField(verbose_name=_("icon"), max_length=100)
@@ -26,7 +29,10 @@ class Account(models.Model):
     class Meta:
         verbose_name = _("account")
         verbose_name_plural = _("accounts")
-        ordering = ("user", "institution", "name")
+        ordering = ("institution", "pos", "name")
 
     def __str__(self):
         return self.name
+
+    def __repr__(self):
+        return f"<Account(user={self.user!r}, name={self.name!r}, institution={self.institution!r}, balance={self.balance!r})>"
