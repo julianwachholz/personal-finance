308452e43ff302fce383bd09d9253bef2eef288a
Julian Wachholz <julian.wachholz@styx7.com>
2019-12-04 10:46:49 +0100

    Show color in category admin list

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from mptt.admin import DraggableMPTTAdmin
 
 from .models import Category
@@ -7,4 +9,18 @@ from .models import Category
 @admin.register(Category)
 class CategoryAdmin(DraggableMPTTAdmin):
     mptt_level_indent = 20
-    list_display = ("tree_actions", "indented_title", "icon", "color")
+    list_display = ("tree_actions", "indented_title", "icon", "color_display")
+
+    class Media:
+        js = (
+            "colorfield/jscolor/jscolor.js"
+            if settings.DEBUG
+            else "colorfield/jscolor/jscolor.min.js",
+        )
+
+    @mark_safe
+    def color_display(self, obj):
+        if obj.color:
+            return f'<input type="text" class="jscolor" value="{obj.color}" disabled readonly />'
+
+    color_display.short_description = "Color"
