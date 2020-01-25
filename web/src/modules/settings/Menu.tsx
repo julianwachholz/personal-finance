aa9f39a93646f3e4e64b92c8b1dd52fd0d952d02
Julian Wachholz <julian@wachholz.ch>
2020-01-25 14:19:41 +0100

    Translate settings menu

--- web/src/modules/settings/Menu.tsx
+++ web/src/modules/settings/Menu.tsx
@@ -8,20 +8,22 @@ import {
 import { List } from "antd-mobile";
 import React from "react";
 import { isMobile } from "react-device-detect";
+import { useTranslation } from "react-i18next";
 import { Redirect, useHistory } from "react-router";
 import useTitle from "../../utils/useTitle";
 import BaseModule from "../base/BaseModule";
 
 const SettingsMenu = () => {
+  const [t] = useTranslation();
   const history = useHistory();
 
-  useTitle("Settings");
+  useTitle(t("menu.settings"));
   if (!isMobile) {
     return <Redirect to="/settings/preferences" />;
   }
   return (
     <BaseModule
-      title="Settings"
+      title={t("menu.settings")}
       className="module module-list"
       onLeftClick={() => {
         history.go(-1);
@@ -33,35 +35,35 @@ const SettingsMenu = () => {
           arrow="horizontal"
           onClick={() => history.push("/settings/categories")}
         >
-          Categories
+          {t("menu.categories")}
         </List.Item>
         <List.Item
           thumb={<TagsOutlined />}
           arrow="horizontal"
           onClick={() => history.push("/settings/tags")}
         >
-          Tags
+          {t("menu.tags")}
         </List.Item>
         <List.Item
           thumb={<ShopOutlined />}
           arrow="horizontal"
           onClick={() => history.push("/settings/payees")}
         >
-          Payees
+          {t("menu.payees")}
         </List.Item>
         <List.Item
           thumb={<ToolOutlined />}
           arrow="horizontal"
           onClick={() => history.push("/settings/preferences")}
         >
-          Preferences
+          {t("menu.preferences")}
         </List.Item>
         <List.Item
           thumb={<UserOutlined />}
           arrow="horizontal"
           onClick={() => history.push("/settings/user")}
         >
-          Profile
+          {t("menu.profile")}
         </List.Item>
       </List>
     </BaseModule>
