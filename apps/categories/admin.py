4af8c49ba6b288e0d429a224bf86b7484c3d4638
Julian Wachholz <julian@wachholz.ch>
2020-02-02 17:12:17 +0100

    Add user to category admin

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -9,6 +9,6 @@ from .models import Category
 @admin.register(Category)
 class CategoryAdmin(AutocompleteFilterMixin, DraggableMPTTAdmin):
     mptt_level_indent = 20
-    list_display = ("tree_actions", "indented_title", "icon", "color")
+    list_display = ("tree_actions", "indented_title", "icon", "color", "user")
     list_filter = (("user", AutocompleteListFilter),)
     search_fields = ("name",)
